6cc048c4651fc66375f4361de863a08b8bc3cb45
Julieta Pellettieri <Joules@uba.ar>
2025-08-25 14:25:22 -0300

    modelos semillas twedee

--- semillas.R
+++ semillas.R
@@ -18,6 +18,7 @@ library(ggplot2)
 library(paletteer)
 library(emmeans)
 library(multcomp)
+library(multcompView)
 # Paleta de colores 
 cols <- paletteer_d("ggthemes::excel_Depth")
 cols_mod <- cols
@@ -60,8 +61,7 @@ ggplot(datos_ratio, aes(x = Línea, y = `%MR`, fill = Localidad)) +
   theme_bw()
 #####                    MODELOS DMR                                    #######
 ## Hay dif sig entre lineas? #sin relastivizar ####
-DMR_Linea <-  glmmTMB(DMR ~ Línea + (1|Localidad) + (1|Año),
-                      ziformula = ~1, family = nbinom2, data = crudos) #ziformula porque hay tantos 0 que rompe poisson y nbinom2 porque poisson no se banca el darma
+DMR_Linea <- lmer(DMR ~ Línea + (1|Localidad) + (1|Año), data = crudos) #ziformula porque hay tantos 0 que rompe poisson y nbinom2 porque poisson no se banca el darma
 #supuestos
 res <- simulateResiduals(DMR_Linea)
 plot(res)
@@ -94,16 +94,17 @@ REL_DMR_Linea <- lmer(DMR ~ Línea + (1|Localidad) + (1|Año), data = relativos)
 summary(REL_DMR_Linea) 
 anova(REL_DMR_Linea)  #Si
 Remm_lineas <- emmeans(REL_DMR_Linea, ~ Línea) #contraste
-pairs(Remm_lineas, adjust = "tukey") #J7-L37 y J7-UF93 de peso se mantienen las dif. la k14-L37 en este caso es MARGINAL en el caso anterior no
+pairs(Remm_lineas, adjust = "tukey") #J7-L37 y J7-UF93 de pedo se mantienen las dif. la k14-L37 en este caso es MARGINAL en el caso anterior no
 #grafiquito
+
 Remm_lineas <- emmeans(REL_DMR_Linea, ~ Línea) # Medias estimadas de DMR por Línea
-Rcld_lineas <- multcomp::cld(Remm_lineas, Letters = letters, adjust = "tukey")
-Rdf_plot <- as.data.frame(Rcld_lineas)
+#Rcld_lineas <- cld(Remm_lineas, Letters = letters, adjust = "tukey")
+Rdf_plot <- as.data.frame(Remm_lineas)
 
 ggplot(Rdf_plot, aes(x = Línea, y = emmean, fill = Línea)) + geom_col(color = "black") + geom_errorbar(aes(ymin = lower.CL, ymax = upper.CL),
                                                                                                       width = 0.2, size = 0.8) +
-  geom_text(aes(label = .group, y = emmean + 5),   # ajustá el +5 según escala de DMR
-            size = 5) +
+  #geom_text(aes(label = .group, y = emmean + 5),   # ajustá el +5 según escala de DMR
+  #          size = 5) +
   scale_fill_manual(values = cols_mod) +
   labs(
     x = "Línea",
@@ -125,11 +126,80 @@ ggplot(Rdf_plot, aes(x = Línea, y = emmean, fill = Línea)) + geom_col(color = 
 
 ## Hay dif entre localidades? # sin relativizar 
 # Modelo con Localidad como VE y Línea/Año como efectos aleatorios
-modelo_loc <- lmer(DMR ~ Localidad + (1|Línea) + (1|Año), data = crudos)
+modelo_loc <- glmmTMB(
+  DMR ~ Localidad+ (1|Línea) + (1|Año),
+  family = tweedie(link="log"),
+  data = relativos
+)
+
+res <- simulateResiduals(modelo_loc)
+plot(res)
+testDispersion(res)#  Test de sobredispersión
+testZeroInflation(res)#  Test de cero-inflación (exceso de ceros)
+
+summary(modelo_loc)
+car::Anova(modelo_loc) # Localidad 43.679  3  1.765e-09 ***
+
+########### Falta tukey ####
+# Medias estimadas
+emm_loc <- emmeans(modelo_loc, ~ Localidad, type = "response") 
+emm_loc
+# Obtenemos los intervalos de confianza
+df_plot_loc <- as.data.frame(emm_loc)
+df_plot_loc$.group <- cld_loc$.group  # agregamos las letras
+
+# Paleta de colores
+cols <- paletteer_d("ggthemes::excel_Depth")
+cols_mod <- cols
+cols_mod[2] <- cols[6]
+
+# Gráfico
+ggplot(df_plot_loc, aes(x = Localidad, y = response , fill = Localidad)) +
+  geom_col(color = "black") +
+  geom_errorbar(aes(ymin = asymp.LCL, ymax = asymp.UCL),
+                width = 0.2, size = 0.8) +
+  geom_text(aes(label = .group, y = response  + 2), size = 5) +  # ajusté +2 para que las letras queden dentro del eje
+  scale_fill_manual(values = cols_mod) +
+  scale_y_continuous(limits = c(0, 1)) +  # ajusta según rango real
+  labs(
+    x = "Localidad",
+    y = "Densidad de inflorescencias (DMR, predicho)",
+    title = "Medias estimadas de DMR por localidad"
+  ) +
+  theme_minimal()
+
+
+
+#### Interaccion genotipo ambiente #### 
+# Modelo con Localidad como VE y Línea/Año como efectos aleatorios
+# modelo_GEI <- lmer(DMR ~ Localidad*Línea + (1|Año), data = relativos) # 
+# 
+# #supuestos
+# # Residuos del modelo
+# residuos <- residuals(modelo_GEI)
+# shapiro.test(residuos) # no me quedan dudas xd  p-value = 2.723e-07
+
+library(glmmTMB)
+modelo_GEI <- glmmTMB(
+  DMR ~ Localidad*Línea + (1|Año),
+  family = tweedie(link="log"),
+  data = relativos
+)
+res <- simulateResiduals(modelo_GEI)
+plot(res)
+testDispersion(res)#  Test de sobredispersión
+testZeroInflation(res)#  Test de cero-inflación (exceso de ceros)
+
 
 # Medias estimadas de DMR por Localidad
-emm_loc <- emmeans(modelo_loc, ~ Localidad)
+emm_GEI <- emmeans(modelo_GEI, ~ Localidad)
 
+
+
+summary(modelo_GEI) 
+anova(modelo_GEI)  #Si
+emm_lineas <- emmeans(modelo_GEI, ~ Línea) #contraste
+pairs(emm_lineas, adjust = "tukey") #J7-L37 y J7-UF93
 # Comparaciones post hoc con letras
 cld_loc <- multcomp::cld(emm_loc, Letters = letters, adjust = "tukey")
 df_plot_loc <- as.data.frame(cld_loc)
@@ -152,6 +222,5 @@ ggplot(df_plot_loc, aes(x = Localidad, y = emmean, fill = Localidad)) +
     y = "Densidad de inflorescencias (DMR, predicho)",
     title = "Medias estimadas de DMR por localidad"
   ) +
-  theme_minimal(base_size = 13)
-
+  theme_minimal()
 
